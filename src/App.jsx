b3db51ca555026c241cbff7b27319b170b2e89e0
Jacob Crider <jcrider@netjets.com>
2024-10-04 10:30:20 -0400

    CGTX-564 pushing header changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { Row } from "react-bootstrap";
-import InteriorAircraftPage from "./InteriorAircraftPage.jsx";
-import logo from "./images/NetJetslogo_2022_smaller.png";
+import njLogo from "./images/njLogo.svg";
 import SearchPage from "./SearchAircraft.jsx";
 
 function App() {
@@ -11,30 +10,29 @@ function App() {
         <header
           style={{
             color: "white",
-            // background: "#11275D",
+            background: "#11275D",
             display: "flex",
-            justifyContent: "space-between", // Keeps logo on the left and h1 on the right
-            alignItems: "center", // Vertically centers content
-            padding: "10px 20px", // Padding around the content
-            backgroundColor: "#f5f5f5",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "10px 20px",
             borderBottom: "2px solid #ccc",
-            width: "100%", // Ensures the header takes up the full width
-            boxSizing: "border-box", // Ensures padding is included in the width
-            position: "relative", // No absolute positioning
-            overflow: "hidden", // Prevents any overflow content
+            width: "100%",
+            boxSizing: "border-box",
+            position: "relative",
+            overflow: "hidden",
           }}
         >
-          <img src={logo} alt="Company Logo" style={{ height: "50px" }} />
+          <img src={njLogo} alt="Company Logo" style={{ height: "50px" }} />
 
           <h1
             style={{
-              fontSize: "1.5rem", // Modern font size
-              fontWeight: "bold", // Bold for emphasis
-              color: "#333", // Subtle modern text color
-              margin: 0, // Removes default margin
-              whiteSpace: "nowrap", // Prevents text from wrapping to a new line
-              overflow: "hidden", // Prevents the text from spilling out
-              textOverflow: "ellipsis", // Adds ellipsis if text overflows
+              fontSize: "1.5rem",
+              fontWeight: "bold",
+              color: "white",
+              margin: 0,
+              whiteSpace: "nowrap",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
             }}
           >
             Aircraft Comparison Tool
